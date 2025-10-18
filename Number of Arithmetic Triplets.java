eeb0ce72a25b0e13e8f3c0c7efca62bb927847c2
Gopika-Bala-Murugan-creator <gopikabalamurugan05@gmail.com>
2025-10-18 19:52:36 +0530

    Add files via upload

---
+++ Number of Arithmetic Triplets.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        Set<Integer> set = new HashSet<>();
+        int count =0;
+
+        for(int num:nums){
+            if(set.contains(num-diff)&&set.contains(num-2*diff)){
+                count++;
+            }
+            set.add(num);
+        }
+        return count;
+    }
+}
