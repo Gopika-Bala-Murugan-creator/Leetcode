80b3aef4a23b1f9a335aa88aecd721045d02c0f7
Gopika-Bala-Murugan-creator <gopikabalamurugan05@gmail.com>
2025-10-28 21:12:18 +0530

    Add files via upload

---
+++ Count Asterisks.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count =0;
+        boolean betweenBars = false;
+
+        for(char c:s.toCharArray()){
+            if(c=='|'){
+                betweenBars=!betweenBars;
+            }
+            else if(c=='*' && !betweenBars){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
